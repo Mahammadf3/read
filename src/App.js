b02a393388c6b21e702b1960791f7a1dcd0f4df1
Mahammadf3 <shaikmahammad622@gmail.com>
2023-08-05 21:16:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+const text="Cricket is a team sport played between two teams of eleven players each. It is a bat-and-ball game played on a roughly oval grass field, in the centre of which is a flat strip of ground 20.12 m (22 yards) long, called a pitch. At each end of the pitch is a set of wooden stumps, called a wicket"
+const App=()=>{
+  const[name,setBool]=useState(true)
+
+  const change=()=>{
+    setBool(prevState=>!prevState)
+  }
+
+return(
+  <div className="container">
+    <img src="https://assets.ccbp.in/frontend/hooks/react-hooks-img.png" alt="logo" className="h"/>
+    {name?<p>{text.slice(0,30)}</p>:<p>{text}</p>}
+    <button type="button"onClick={change}>
+      {name?"ReadMore":"ReadLess"}
+    </button>
+  </div>
+)
 }
-
-export default App;
+export default App
